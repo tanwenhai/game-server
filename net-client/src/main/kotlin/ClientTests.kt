501f628e5f5809d1fd48ee48d3b3ffe39bba7388
tanwenhai <tanwenhai@outlookcom>
2019-04-14 23:21:26 +0800

    vert.x

--- net-client/src/main/kotlin/ClientTests.kt
+++ net-client/src/main/kotlin/ClientTests.kt
@@ -5,9 +5,35 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioSocketChannel
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+import io.vertx.core.buffer.Buffer
+import io.vertx.kotlin.core.net.netClientOptionsOf
 import java.util.concurrent.TimeUnit
 
-class ClientTests
+class ClientTests : AbstractVerticle() {
+    override fun start(startFuture: Future<Void>?) {
+        val options = netClientOptionsOf(
+                connectTimeout = 10000)
+        val client = vertx.createNetClient(options)
+        client.connect(4321, "localhost") { res ->
+            if (res.succeeded()) {
+                println("Connected!")
+                val socket = res.result()
+                socket.handler {buffer->
+                    print("recv $buffer")
+                    val buf = Unpooled.buffer()
+                    buf.writeShort(ServerType.ROOM.value.toInt())
+                    buf.writeInt(4)
+                    buf.writeCharSequence("haha", Charsets.UTF_8)
+                    socket.write(Buffer.buffer(buf))
+                }
+            } else {
+                println("Failed to connect: ${res.cause().message}")
+            }
+        }
+    }
+}
 
 fun main() {
     val b = Bootstrap()
@@ -16,13 +42,7 @@ fun main() {
             .channel(NioSocketChannel::class.java)
             .handler(object : ChannelInboundHandlerAdapter() {
                 override fun channelActive(ctx: ChannelHandlerContext) {
-                    val buf = Unpooled.buffer()
-                    buf.writeShort(ServerType.ROOM.value.toInt())
-                    buf.writeInt(4)
-                    buf.writeCharSequence("haha", Charsets.UTF_8)
-                    ctx.writeAndFlush(buf).addListener {
-                        println("send success")
-                    }
+
                 }
 
                 override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
